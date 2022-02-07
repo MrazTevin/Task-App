40d4970f54bc945dbb44090b269ff4232b05a33f
Mraz Tevin <millatevin@gmail.com>
2022-02-07 17:46:37 +0300

    add form elements

--- src/Task.js
+++ src/Task.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Form = () => {
+    <div className="container_input">
+        <div className="button_input">
+            <input type ="text"/>
+            <button>ADD</button>
+
+        </div>
+    </div>
 
 }
 
-export default Forms
+export default Form
