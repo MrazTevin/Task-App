c92c587714a5ab1d105452c8d0c6ad7f21a8b52d
Mraz Tevin <millatevin@gmail.com>
2022-02-07 17:55:55 +0300

    add return statement

--- src/Form.js
+++ src/Form.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
 const Form = () => {
-    <div className="container_input">
-        <div className="button_input">
-            <input type ="text"/>
-            <button>ADD</button>
 
+    return (
+        <div className="container_input">
+            <div className="button_input">
+                <input type ="text"/>
+                <button>ADD</button>
+
+            </div>
         </div>
-    </div>
+    );
 
 }
 
