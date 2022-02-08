139f4f25381a26879e9329859c980b32bec8ae35
Mraz Tevin <millatevin@gmail.com>
2022-02-08 11:03:00 +0300

    declare input state variable

--- src/Form.js
+++ src/Form.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Form = () => {
-
+    const [inputText,setInputText] = useState(' ');
     return (
         <div className="container_input">
             <div className="button_input">
