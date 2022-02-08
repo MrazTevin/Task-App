fb42df16f6e2b860d40f895cf90154fa6e353460
Mraz Tevin <millatevin@gmail.com>
2022-02-08 14:52:36 +0300

    create and pass prop function

--- src/Form.js
+++ src/Form.js
@@ -2,12 +2,18 @@ import React, { useState } from 'react';
 
 const Form = () => {
     const [inputText,setInputText] = useState(' ');
+
+    const displayValue = e => {
+        setInputText(e.target.value)
+        console.log(e.target.value);
+    }
+    
     return (
         <div className="container_input">
             <div className="button_input">
-                <input type ="text"/>
+                <input type="text" value={inputText} onChange={displayValue}/>
                 <button>ADD</button>
-
+                
             </div>
         </div>
     );
