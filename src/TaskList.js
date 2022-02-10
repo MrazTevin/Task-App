336c4587e4c088e090c33055628783c5a66b1734
Mraz Tevin <millatevin@gmail.com>
2022-02-10 10:43:08 +0300

    create child div inside parent

--- src/TaskList.js
+++ src/TaskList.js
@@ -1,10 +1,18 @@
 import React, { useState } from 'react';
 import { createContext } from 'react'; 
+import Task from './Task';
 
 const TaskList = (todos, setTodos) => {
    
-  return <div></div>;
-
+  return (
+      <div className="list_container">
+          <div className="todos">
+              {todos.map(todo => {
+                  <Task/>
+              })}
+          </div>
+    </div>
+  )
 };
 
 export default TaskList;
